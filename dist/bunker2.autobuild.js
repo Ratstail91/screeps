ba0d50c8cd2e6d3ef4132bb242b7d9bf89bdbf53
Kayne Ruse <kayneruse@gmail.com>
2019-07-10 04:23:32 +1000

    Wiped clean

--- dist/bunker2.autobuild.js
+++
@@ -1,8 +0,0 @@
-module.exports = 
-[
-	{ "x": 1, "y": -2, "structureType": "extension" },
-	{ "x": 2, "y": -1, "structureType": "extension" },
-	{ "x": 2, "y": 0, "structureType": "extension" },
-	{ "x": 2, "y": 1, "structureType": "extension" },
-	{ "x": 1, "y": 2, "structureType": "extension" }
-]
