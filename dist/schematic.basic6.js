7891d2c584314da49925e26cb522f7ef37378064
Kayne Ruse <kayneruse@gmail.com>
2019-07-17 13:53:30 +1000

    Tweaked schematics

--- dist/schematic.basic6.js
+++ dist/schematic.basic6.js
@@ -4,11 +4,11 @@ module.exports =
 	{ "x": 3, "y": -5, "structureType": "extension"},
 	{ "x": 4, "y": -4, "structureType": "extension"},
 	{ "x": 5, "y": -3, "structureType": "extension"},
-	{ "x": 5, "y": -1, "structureType": "extension"},
+	{ "x": 6, "y": -1, "structureType": "extension"},
 
-	{ "x": 5, "y": 1, "structureType": "extension"},
+	{ "x": 6, "y": 1, "structureType": "extension"},
 	{ "x": 5, "y": 3, "structureType": "extension"},
 	{ "x": 4, "y": 4, "structureType": "extension"},
 	{ "x": 3, "y": 5, "structureType": "extension"},
-	{ "x": 2, "y": 6, "structureType": "extension"}
+	{ "x": 1, "y": 6, "structureType": "extension"},
 ]
