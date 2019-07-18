91f9f4502867ddc02e5d6a56ad2240cf0909ea3d
Kayne Ruse <kayneruse@gmail.com>
2019-07-19 05:50:02 +1000

    Tweaked restockers

--- dist/spawns.spawners.js
+++ dist/spawns.spawners.js
@@ -118,8 +118,9 @@ function spawnColonist(spawn, body, extraTags = []) {
 
 //bespoke
 function spawnRestocker(spawn, body, extraTags = []) {
-	return spawnCreep(spawn, 'restocker', [PICKUP, DEPOSIT, WITHDRAW], body, ['restocker', ...extraTags], {
+	return spawnCreep(spawn, 'restocker', [DEPOSIT, PICKUP, WITHDRAW], body, ['restocker', ...extraTags], {
 		DEPOSIT: {
+			returnHomeFirst: true,
 			stores: [TOWER, SPAWN, EXTENSION, TERMINAL]
 		},
 		WITHDRAW: {
