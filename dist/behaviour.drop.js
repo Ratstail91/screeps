00853b0a471ed6c83dcf512a59ae6291a2432d22
Kayne Ruse <kayneruse@gmail.com>
2019-10-16 02:53:59 +1100

    Fixed a deprecated call

--- dist/behaviour.drop.js
+++ dist/behaviour.drop.js
@@ -1,7 +1,7 @@
 const { DROP: BEHAVIOUR_NAME } = require('behaviour_names');
 
 function run(creep) {
-	if (creep.carry[RESOURCE_ENERGY] != 0) {
+	if (creep.store[RESOURCE_ENERGY] != 0) {
 		creep.drop(RESOURCE_ENERGY);
 		return false;
 	}
